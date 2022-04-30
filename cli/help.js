e901b249a3e21136048ecfdb6bbc9ae4ba0c1c5c
Felippe Regazio <felippe.moraes@zoho.com>
2022-04-30 15:57:50 -0300

    changed package name

--- cli/help.js
+++ cli/help.js
@@ -1,11 +1,3 @@
 console.log(`
-usage:
-  node blog {command}|{postname}
-
-commands:
-  help: show this help
-  update: update the homepage post feed
-
-node blog {postname}:
-  creates a new post at/posts where {postname} is the post filename/uri 
+  help
 `);
