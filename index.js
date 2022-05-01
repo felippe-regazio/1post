713ebe49cf9e732e03eb8047df69a8fa9075f460
Felippe Regazio <felippe.moraes@zoho.com>
2022-05-01 11:26:50 -0300

    added no command help msg

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const args = process.argv.slice(2);
 const commands = [ 'help', 'start', 'build', 'serve' ];
 
+if (!args[0]) {
+  require('./cli/help.js');
+  process.exit(0);
+}
+
 commands.includes(args[0]) ?
   require(`./cli/${args[0]}.js`) :
   require('./cli/post.js');
